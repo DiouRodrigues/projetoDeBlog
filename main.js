6363eb1e87c81b78afc3172759cd213280ea0c43
Diovane Rodrigues <diovanerodriguees@outlook.com>
2023-02-27 20:10:52 -0300

    filtro que filtra

--- main.js
+++ main.js
@@ -14,3 +14,37 @@ function proximaImg(){
         document.getElementById('radio'+cont).checked = true;
 
 }
+
+const filter =document.querySelector("header input")
+const box = document.querySelectorAll('.aboutPages .box ')
+
+filter.addEventListener('input', filterBox)
+
+function filterBox(){
+    if(filter.value !=''){
+        for(let bo of box){
+
+            let title = bo.querySelector('a')
+            title = title.textContent.toLowerCase()
+            
+                let filterText =filter.value.toLowerCase()
+
+                if(!title.includes(filterText)){
+                    bo.style.display="none"
+
+                }
+
+                else{
+                    bo.style.display="block"
+
+                }
+        }
+
+    }
+    else{
+        for(let bo of box){
+            bo.style.display="block"
+        }
+
+    }
+}
